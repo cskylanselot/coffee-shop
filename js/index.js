2bc9fb10099a71c469c38749e6effe3a31feddf0
Ihor kustov <lanselot864@gmail.com>
2025-06-10 16:12:40 +0300

    Работал с slider/js и начал делать меню для покупки товаров

--- js/index.js
+++ js/index.js
@@ -3,23 +3,103 @@
 const sliderMain = document.querySelector(".sliderMain");
 const arrowLeft = document.querySelector(".arrowLeft");
 const arrowRight = document.querySelector(".arrowRight");
+const control1 = document.querySelector(".control1");
+const control2 = document.querySelector(".control2");
+const control3 = document.querySelector(".control3");
 let offset = 0;
-arrowRight.addEventListener("click", function () {
+
+arrowRight.addEventListener("click", nextSlide);
+
+function nextSlide() {
   offset = offset + 50;
   if (offset > 100) {
     offset = 0;
   }
   sliderMain.style.left = -offset + "rem";
-});
-arrowLeft.addEventListener("click", function () {
+  if (offset == 0) {
+    control1.classList.add("controlActive");
+    control2.classList.remove("controlActive");
+    control3.classList.remove("controlActive");
+  }
+  if (offset == 50) {
+    control2.classList.add("controlActive");
+    control1.classList.remove("controlActive");
+    control3.classList.remove("controlActive");
+  }
+  if (offset == 100) {
+    control3.classList.add("controlActive");
+    control2.classList.remove("controlActive");
+    control1.classList.remove("controlActive");
+  }
+}
+
+arrowLeft.addEventListener("click", prevSlide);
+function prevSlide() {
   offset = offset - 50;
   if (offset < 0) {
     offset = 100;
   }
   sliderMain.style.left = -offset + "rem";
-});
+  if (offset == 0) {
+    control1.classList.add("controlActive");
+    control2.classList.remove("controlActive");
+    control3.classList.remove("controlActive");
+  }
+  if (offset == 50) {
+    control2.classList.add("controlActive");
+    control1.classList.remove("controlActive");
+    control3.classList.remove("controlActive");
+  }
+  if (offset == 100) {
+    control3.classList.add("controlActive");
+    control2.classList.remove("controlActive");
+    control1.classList.remove("controlActive");
+  }
+}
+
+let avtoSlider;
+
+function startSlider() {
+  avtoSlider = setInterval(nextSlide, 10000);
+}
+
+function stopSlider() {
+  clearInterval(avtoSlider);
+}
+
+startSlider();
+
+sliderMain.addEventListener("mouseover", stopSlider);
+sliderMain.addEventListener("mouseout", startSlider);
 
 //Конец Slider
+
+// menu click
+const coffeeMenuMain = document.querySelector(".coffee-menuMain");
+const heroMenu = document.querySelector(".heroMenu");
+const hero = document.querySelector(".hero");
+const header = document.querySelector(".header");
+const mainFirst = document.querySelector(".mainFirst");
+let flag = true;
+
+function menuOpen() {
+  if (flag) {
+    header.style.height = 3.75 + "rem";
+    hero.style.display = "none";
+    mainFirst.style.display = "none";
+    mainSecond.style.display = "flex";
+    flag = false;
+  } else {
+    header.style.height = 45.25 + "rem";
+    hero.style.display = "flex";
+    mainFirst.style.display = "block";
+    mainSecond.style.display = "none";
+    flag = true;
+  }
+}
+coffeeMenuMain.addEventListener("click", menuOpen);
+heroMenu.addEventListener("click", menuOpen);
+// menu click end
 
 // Coffee Tea Dessert menu
 const mainSecond = document.querySelector(".mainSecond");
