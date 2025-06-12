5701fc412c9101d880eb8680a5a2360874e2cf8d
Ihor kustov <lanselot864@gmail.com>
2025-06-12 19:06:17 +0300

    доделывал меню покупки кофе пока не офнули свет

--- js/index.js
+++ js/index.js
@@ -79,6 +79,7 @@ const coffeeMenuMain = document.querySelector(".coffee-menuMain");
 const heroMenu = document.querySelector(".heroMenu");
 const hero = document.querySelector(".hero");
 const header = document.querySelector(".header");
+const StartMenuBack = document.querySelector(".StartMenuBack");
 const mainFirst = document.querySelector(".mainFirst");
 let flag = true;
 
@@ -97,8 +98,10 @@ function menuOpen() {
     flag = true;
   }
 }
+
 coffeeMenuMain.addEventListener("click", menuOpen);
 heroMenu.addEventListener("click", menuOpen);
+StartMenuBack.addEventListener("click", menuOpen);
 // menu click end
 
 // Coffee Tea Dessert menu
@@ -140,17 +143,188 @@ dessertTabsMain.addEventListener("click", function () {
 
 // Coffee Buy Menu click
 const coffeeGrid1 = document.querySelector(".coffeeGrid1");
+const coffeeGrid2 = document.querySelector(".coffeeGrid2");
+const coffeeGrid3 = document.querySelector(".coffeeGrid3");
+const coffeeGrid4 = document.querySelector(".coffeeGrid4");
+const coffeeGrid5 = document.querySelector(".coffeeGrid5");
+const coffeeGrid6 = document.querySelector(".coffeeGrid6");
+const coffeeGrid7 = document.querySelector(".coffeeGrid7");
+const coffeeGrid8 = document.querySelector(".coffeeGrid8");
 const backDrop = document.querySelector(".backDrop");
-const coffeeBuyMenuClose = document.querySelector(".coffeeBuyMenuClose");
+const coffeeBuyMenuClose = document.querySelectorAll(".coffeeBuyMenuClose");
 const IrishID = document.getElementById("IrishID");
+const KahluaID = document.getElementById("KahluaID");
+const HoneyRafID = document.getElementById("HoneyRafID");
+const IceCappuccinoID = document.getElementById("IceCappuccinoID");
+const EspressoID = document.getElementById("EspressoID");
+const LatteID = document.getElementById("LatteID");
+const LatteMacchiatoID = document.getElementById("LatteMacchiatoID");
+const CoffeeWithCognacID = document.getElementById("CoffeeWithCognacID");
 
-coffeeBuyMenuClose.addEventListener("click", function () {
-  backDrop.style.display = "none";
-  IrishID.style.display = "none";
-});
 coffeeGrid1.addEventListener("click", function () {
   backDrop.style.display = "flex";
   IrishID.style.display = "flex";
 });
+coffeeGrid2.addEventListener("click", function () {
+  backDrop.style.display = "flex";
+  KahluaID.style.display = "flex";
+});
+coffeeGrid3.addEventListener("click", function () {
+  backDrop.style.display = "flex";
+  HoneyRafID.style.display = "flex";
+});
+coffeeGrid4.addEventListener("click", function () {
+  backDrop.style.display = "flex";
+  IceCappuccinoID.style.display = "flex";
+});
+coffeeGrid5.addEventListener("click", function () {
+  backDrop.style.display = "flex";
+  EspressoID.style.display = "flex";
+});
+coffeeGrid6.addEventListener("click", function () {
+  backDrop.style.display = "flex";
+  LatteID.style.display = "flex";
+});
+coffeeGrid7.addEventListener("click", function () {
+  backDrop.style.display = "flex";
+  LatteMacchiatoID.style.display = "flex";
+});
+coffeeGrid8.addEventListener("click", function () {
+  backDrop.style.display = "flex";
+  CoffeeWithCognacID.style.display = "flex";
+});
+
+coffeeBuyMenuClose.forEach(function (close) {
+  close.addEventListener("click", function () {
+    backDrop.style.display = "none";
+    IrishID.style.display = "none";
+    KahluaID.style.display = "none";
+    HoneyRafID.style.display = "none";
+    IceCappuccinoID.style.display = "none";
+    EspressoID.style.display = "none";
+    LatteID.style.display = "none";
+    LatteMacchiatoID.style.display = "none";
+    CoffeeWithCognacID.style.display = "none";
+  });
+});
+// Coffee Buy Menu Button click
+const SizeS = document.querySelectorAll(".SizeS");
+const SizeSText = document.querySelectorAll(".SizeSText");
+const SizeSIcon = document.querySelectorAll(".SizeSIcon");
+const SizeM = document.querySelectorAll(".SizeM");
+const SizeMText = document.querySelectorAll(".SizeMText");
+const SizeMIcon = document.querySelectorAll(".SizeMIcon");
+const SizeL = document.querySelectorAll(".SizeL");
+const SizeLText = document.querySelectorAll(".SizeLText");
+const SizeLIcon = document.querySelectorAll(".SizeLIcon");
+
+const Additives1 = document.querySelectorAll(".Additives1");
+const AdditivesIcon1 = document.querySelectorAll(".AdditivesIcon1");
+const AdditivesText1 = document.querySelectorAll(".AdditivesText1");
+const Additives2 = document.querySelectorAll(".Additives2");
+const AdditivesIcon2 = document.querySelectorAll(".AdditivesIcon2");
+const AdditivesText2 = document.querySelectorAll(".AdditivesText2");
+const Additives3 = document.querySelectorAll(".Additives3");
+const AdditivesIcon3 = document.querySelectorAll(".AdditivesIcon3");
+const AdditivesText3 = document.querySelectorAll(".AdditivesText3");
+
+SizeS.forEach((sizeS, index) => {
+  sizeS.addEventListener("click", () => {
+    SizeM[index].classList.remove("SizeActive");
+    SizeMText[index].classList.remove("SizeActive1");
+    SizeMIcon[index].classList.remove("SizeActive2");
+    SizeL[index].classList.remove("SizeActive");
+    SizeLText[index].classList.remove("SizeActive1");
+    SizeLIcon[index].classList.remove("SizeActive2");
+    SizeSIcon[index].classList.remove("SizeActive3");
+    sizeS.classList.add("SizeActive");
+    SizeLIcon[index].classList.add("SizeActive3");
+    SizeMIcon[index].classList.add("SizeActive3");
+    SizeSText[index].classList.add("SizeActive1");
+    SizeSIcon[index].classList.add("SizeActive2");
+  });
+});
+
+SizeM.forEach((sizeM, index) => {
+  sizeM.addEventListener("click", () => {
+    SizeS[index].classList.remove("SizeActive");
+    SizeSText[index].classList.remove("SizeActive1");
+    SizeSIcon[index].classList.remove("SizeActive2");
+    SizeL[index].classList.remove("SizeActive");
+    SizeLText[index].classList.remove("SizeActive1");
+    SizeLIcon[index].classList.remove("SizeActive2");
+    SizeMIcon[index].classList.remove("SizeActive3");
+    sizeM.classList.add("SizeActive");
+    SizeLIcon[index].classList.add("SizeActive3");
+    SizeMText[index].classList.add("SizeActive1");
+    SizeMIcon[index].classList.add("SizeActive2");
+    SizeSIcon[index].classList.add("SizeActive3");
+  });
+});
+
+SizeL.forEach((sizeL, index) => {
+  sizeL.addEventListener("click", () => {
+    SizeM[index].classList.remove("SizeActive");
+    SizeMText[index].classList.remove("SizeActive1");
+    SizeMIcon[index].classList.remove("SizeActive2");
+
+    SizeS[index].classList.remove("SizeActive");
+    SizeSText[index].classList.remove("SizeActive1");
+    SizeSIcon[index].classList.remove("SizeActive2");
+
+    SizeLIcon[index].classList.remove("SizeActive2");
+    SizeLIcon[index].classList.remove("SizeActive3");
+
+    sizeL.classList.add("SizeActive");
+    SizeLText[index].classList.add("SizeActive1");
+    SizeLIcon[index].classList.add("SizeActive2");
+    SizeSIcon[index].classList.add("SizeActive3");
+    SizeMIcon[index].classList.add("SizeActive3");
+  });
+});
+
+Additives1.forEach((additives1, index) => {
+  additives1.addEventListener("click", () => {
+    AdditivesIcon1[index].classList.remove("SizeActive2");
+    AdditivesIcon1[index].classList.remove("SizeActive3");
+    additives1.classList.add("SizeActive");
+    AdditivesText1[index].classList.add("SizeActive1");
+    AdditivesIcon1[index].classList.add("SizeActive2");
+  });
+});
+Additives2.forEach((additives2, index) => {
+  additives2.addEventListener("click", () => {
+    AdditivesIcon2[index].classList.remove("SizeActive3");
+    additives2.classList.add("SizeActive");
+    AdditivesText2[index].classList.add("SizeActive1");
+    AdditivesIcon2[index].classList.add("SizeActive2");
+  });
+});
+
+Additives3.forEach((additives3, index) => {
+  additives3.addEventListener("click", () => {
+    AdditivesIcon3[index].classList.remove("SizeActive2");
+    AdditivesIcon3[index].classList.remove("SizeActive3");
+    additives3.classList.add("SizeActive");
+    AdditivesText3[index].classList.add("SizeActive1");
+    AdditivesIcon3[index].classList.add("SizeActive2");
+  });
+});
+
+coffeeBuyMenuClose.forEach(function (close) {
+  close.addEventListener("click", function () {
+    backDrop.style.display = "none";
+    IrishID.style.display = "none";
+    KahluaID.style.display = "none";
+    HoneyRafID.style.display = "none";
+    IceCappuccinoID.style.display = "none";
+    EspressoID.style.display = "none";
+    LatteID.style.display = "none";
+    LatteMacchiatoID.style.display = "none";
+    CoffeeWithCognacID.style.display = "none";
+  });
+});
+
+// Coffee Buy Menu Button click end
 
 // Coffee Buy Menu click End
